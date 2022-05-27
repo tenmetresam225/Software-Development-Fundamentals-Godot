ca2f56d091a1a15d81ad2ddb579cf26b8b6c8964
Sam <87291184+Jasper-0195551@users.noreply.github.com>
2022-05-27 12:21:22 +1000

    Update Player.gd

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-
+var movement_speed = 200
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,9 +8,11 @@ extends KinematicBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_physics_process(true) 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+func _physics_process(delta):
+if Input.is_action_pressed("ui_left"):
+move_and_collide(vector2(-movment_speed * delta, 0))
+if Input.is_action_pressed("ui_right"):
+	move_and_collide(vector2(movement_speed * delta, 0))
